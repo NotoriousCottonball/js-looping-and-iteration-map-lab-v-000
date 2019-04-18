18221ee9756c816eecbed53e7b4dfa6b8697b23c
George  Pianka <gpianka1@gmail.com>
2019-04-18 07:46:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ function lowerCaseDrivers(drivers) {
   return lcDrivers;
 }
 
+function nameToAttributes(drivers) {
+  const nameObjectDrivers = drivers.map(function(name) {
+    const first = name.split(" ")[0];
+    const last = name.split(" ")[1];
+    return {
+      firstName: first,
+      lastName: last
+    };
+  });
+  return nameObjectDrivers;
+}
