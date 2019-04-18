94a18dc48257bc28311709a450cac3d1d01bae01
George  Pianka <gpianka1@gmail.com>
2019-04-18 07:46:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file.
+
+
+function lowerCaseDrivers(drivers) {
+  const lcDrivers = drivers.map(function(name) {
+    return name.toLowerCase();
+  });
+  return lcDrivers;
+}
+
